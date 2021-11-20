27eff58547cfe30d5f67ddcda35896b1712ae158
Mike Mitchell <alchemistikk@gmail.com>
2021-11-20 10:20:51 -0500

    Add caeser-cipher.rb from repo: basic-ruby-projects

---
+++ caeser-cipher.rb
@@ -0,0 +1,35 @@
+require 'pry-byebug'
+
+ASCII_UPPER_CASE_LOW = 65
+ASCII_UPPER_CASE_HIGH = 90
+ASCII_LOWER_CASE_LOW = 97
+ASCII_LOWER_CASE_HIGH = 122
+
+def shift_ascii_by_int(arr, int)
+  arr.map! do |ascii|
+    if ascii + int > 122
+      ascii = ASCII_LOWER_CASE_LOW + (ascii + int - ASCII_LOWER_CASE_HIGH)
+    elsif ascii < ASCII_LOWER_CASE_LOW && ascii + int > ASCII_UPPER_CASE_HIGH
+      ascii = ASCII_UPPER_CASE_LOW + (ascii + int - ASCII_UPPER_CASE_HIGH)
+    else ascii + int
+    end
+  end
+end
+
+def caeser_cipher(str, int)
+  arr = str.split("")
+  p arr
+  arr.map! { |char| char.ord }
+  shift_ascii_by_int(arr, int)
+  arr.map! { |shifted_ascii| shifted_ascii.chr }
+  arr.join
+end
+
+text_prompt = 'Enter text to be ciphered.'
+shift_prompt = 'Enter the shift length.'
+
+puts text_prompt
+caeser_string = gets.chomp
+puts shift_prompt
+caeser_shift = gets.chomp.to_i
+puts caeser_cipher(caeser_string, caeser_shift)
